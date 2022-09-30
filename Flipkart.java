0272ded94232e42f8e9830b13dae71decb0424c2
SAKTHIVEL00777 <sakthi040797@gmail.com>
2022-09-30 16:53:48 +0530

    codeis success

---
+++ Flipkart.java
@@ -0,0 +1,11 @@
+class Flipkart{
+
+public static void main(String arg[])
+
+{
+   System.out.println("product sales in online purches");
+   
+   System.out.println(" all product is avalaiable ");
+}
+
+}
