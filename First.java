3e05ba8f9352cb79244f453e7b9d613f721d06e4
SAKTHIVEL00777 <113890467+SAKTHIVEL00777@users.noreply.github.com>
2022-11-01 19:19:01 +0530

    Update First.java

--- First.java
+++ First.java
@@ -3,6 +3,7 @@ class First
 public static void main(String arg[])
 {
     System.out.println("WELCOME TO JAVA");  
+    System.out.println ("First");
     System.out.println("JAVA BATCH NO IS 26");
     
     
